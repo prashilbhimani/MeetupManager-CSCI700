fce90848c5def6a52adc838cd4a9beff5eae29ce
prashilbhimani <prashilbhimani24@gmail.com>
2019-02-23 20:22:51 -0700

    Completed GoStream

---
+++ GoStream/meetupStream.go
@@ -0,0 +1,49 @@
+package main
+
+import "net/http"
+import "encoding/json"
+import "fmt"
+//import "log"
+//import "bufio"
+
+import (
+	"context"
+	"reflect"
+	"github.com/segmentio/kafka-go"
+)
+
+
+func KafkaWriter(x string, w *kafka.Writer) {
+	
+	w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte(x),
+		},
+	)
+}
+
+func main() {
+	resp,err := http.Get("http://stream.meetup.com/2/rsvps")
+	decoder:=json.NewDecoder(resp.Body)
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "MyTopic",
+	})
+	fmt.Println(reflect.TypeOf(w).String())
+	if err==nil{
+		for {
+			var myJson interface{}
+			decoder.Decode(&myJson)
+			b, err:=json.MarshalIndent(myJson, "", "    ")
+			if err==nil{
+
+				fmt.Println(string(b))
+				go KafkaWriter(string(b),w)
+			}
+		}
+	}else{
+		fmt.Println("Error : ")
+		fmt.Println(err)
+	}
+}
