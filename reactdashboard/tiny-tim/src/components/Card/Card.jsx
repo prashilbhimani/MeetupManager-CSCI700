4704e43354e508056d232572fecdb91205c8d1f6
Sharan Srivatsa <shsr7814@colorado.edu>
2019-03-04 22:46:58 -0700

    clean up phase 1

--- reactdashboard/tiny-tim/src/components/Card/Card.jsx
+++ reactdashboard/tiny-tim/src/components/Card/Card.jsx
@@ -27,26 +27,6 @@ export class Card extends Component {
             <div className="stats">
               <i className={this.props.statsIcon} /> {this.props.stats}
             </div>
-            <div>
-              {/*TODO: Move pagination to the right corner*/}
-              <Pagination>
-                <Pagination.First />
-                <Pagination.Prev />
-                <Pagination.Item>{1}</Pagination.Item>
-                <Pagination.Ellipsis />
-
-                <Pagination.Item>{10}</Pagination.Item>
-                <Pagination.Item>{11}</Pagination.Item>
-                <Pagination.Item active>{12}</Pagination.Item>
-                <Pagination.Item>{13}</Pagination.Item>
-                <Pagination.Item disabled>{14}</Pagination.Item>
-
-                <Pagination.Ellipsis />
-                <Pagination.Item>{20}</Pagination.Item>
-                <Pagination.Next />
-                <Pagination.Last />
-              </Pagination>;
-            </div>
           </div>
         </div>
 
