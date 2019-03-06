1e1db9c9bd7b382876ae786ee17921dd62c56726
Sharan Srivatsa <shsr7814@colorado.edu>
2019-03-06 10:37:32 -0700

    first set of reducers in place

--- reactdashboard/tiny-tim/src/App.js
+++ reactdashboard/tiny-tim/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
+// import ReactDOM from "react-dom";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import indexRoutes from "routes/index.jsx";
-import { createStore, applyMiddleware } from "redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
+import store from './store';
 
 
-const store = createStore(() => [],{}, applyMiddleware());
+
 
 class App extends Component {
     render() {
--- reactdashboard/tiny-tim/src/components/Posts/Posts.js
+++ reactdashboard/tiny-tim/src/components/Posts/Posts.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
+import { fetchPosts } from '../../actions/postActions';
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: []
-    }
-  }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => this.setState({posts: data}));
+    this.props.fetchPosts();
   }
-  render() {
-    const postItems = this.state.posts.map(post => (
+  render() {    
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -28,4 +21,9 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+// posts is what we set in the post reducer
+
+export default connect(mapStateToProps, {fetchPosts })(Posts);
