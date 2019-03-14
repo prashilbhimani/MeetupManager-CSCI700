faffa6be43d3533143f576a38376ecfe3c4081cd
Sharan Srivatsa <shsr7814@colorado.edu>
2019-03-13 19:20:58 -0600

    basic landing page in place

--- reactdashboard/tiny-tim/src/App.js
+++ reactdashboard/tiny-tim/src/App.js
@@ -9,26 +9,77 @@ import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 import store from './store';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import LandingPage from "./views/LandingPage/Landingpage";
 
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCdQ0Ywgh2EHFmArK-ektjrM_VLqJ1WMWQ",
+  authDomain: "sharan-firebase-auth-928f8.firebaseapp.com"
+})
 
 
 class App extends Component {
-    render() {
-        return (
-  <Provider store={store}>
-    <HashRouter>
-      <Switch>
-        {indexRoutes.map((prop, key) => {
-          // let myprop = JSON.stringify(prop);
-          // Here key is refering to the index. It starts from 0. Since theres just 1 element it will be zero.
-          // prop is referring to the entire JSON object in routes/index.js      
-          return <Route to={prop.path} component={prop.component} key={key} />;        
-        })}
-      </Switch>
-    </HashRouter>
-  </Provider>
-  )
+  state = {
+    isSignedIn: false
+  }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        isSignedIn: !!user
+      })
+      console.log("user", user)
+    });
+  }
+
+//     render() {
+//         return (
+//   <Provider store={store}>
+//     <HashRouter>
+//       <Switch>
+//         {indexRoutes.map((prop, key) => {
+//           // let myprop = JSON.stringify(prop);
+//           // Here key is refering to the index. It starts from 0. Since theres just 1 element it will be zero.
+//           // prop is referring to the entire JSON object in routes/index.js      
+//           return <Route to={prop.path} component={prop.component} key={key} />;        
+//         })}
+//       </Switch>
+//     </HashRouter>
+//   </Provider>
+//   )
+// } 
+// <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()} />
+render() {
+  return (
+  <div className= "App">
+  {
+    this.state.isSignedIn 
+    ? 
+    <span>
+      <div> Signed in </div>
+      <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+      <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+      <img alt="profile picture" src={firebase.auth().currentUser.photoURL}/>
+    </span>
+     :
+     <span>
+      <LandingPage/>       
+     </span>
+  }
+  </div>
+  );
 }
 }
 
---
+++ reactdashboard/tiny-tim/src/views/LandingPage/Landingpage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Nav from "../../components/Nav/Nav";
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="content">
+        <Nav/>               
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
