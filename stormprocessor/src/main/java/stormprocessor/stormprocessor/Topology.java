df8ef981750affcd38255d7d22e79c5bd79c8724
Mahesh Parab <maheshp2812@gmail.com>
2019-03-31 20:40:05 -0600

    Stormprocessor works with GCP

--- stormprocessor/src/main/java/stormprocessor/stormprocessor/Topology.java
+++ stormprocessor/src/main/java/stormprocessor/stormprocessor/Topology.java
@@ -1,8 +1,10 @@
 package stormprocessor.stormprocessor;
 
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.kafka.spout.KafkaSpout;
+import org.apache.storm.kafka.spout.KafkaSpoutConfig;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.Config;
-import org.apache.storm.LocalCluster;
 import org.apache.storm.mongodb.bolt.MongoInsertBolt;
 import org.apache.storm.mongodb.common.mapper.MongoMapper;
 import org.apache.storm.mongodb.common.mapper.SimpleMongoMapper;
@@ -11,18 +13,29 @@ public class Topology {
 	public void create() {
 		TopologyBuilder builder = new TopologyBuilder();
 		Config config = new Config();
-		
-		builder.setSpout("KafkaSpout", new TwitterStreamKafkaSpoutImitation(), 2);
+
+		//SpoutConfig kafkaConfig = new SpoutConfig(new ZkHosts("kafka-broker:9092"), "Meetups", "", "id");
+//		builder.setSpout("KafkaSpout", new KafkaSpout(kafkaConfig), 1);
+		builder.setSpout("KafkaSpout", new KafkaSpout<>(KafkaSpoutConfig.builder("kafka-broker:9092", "Meetups").setGroupId("id").build()), 1);
 		builder.setBolt("JSONBolt", new JSONBolt(), 3).shuffleGrouping("KafkaSpout");
 		
-		String url = "mongodb://127.0.0.1:27017/testdb";
+		String url = "mongodb://mongo-db:27017/testdb";
     	String collectionName = "test";
     	
     	MongoMapper mongoMapper = new SimpleMongoMapper().withFields("name", "nested");
     	MongoInsertBolt mongoInsertBolt = new MongoInsertBolt(url, collectionName, mongoMapper);
     	builder.setBolt("MongoInsertBolt", mongoInsertBolt).shuffleGrouping("JSONBolt");
-    	
-    	LocalCluster cluster = new LocalCluster();
-    	cluster.submitTopology("meetup-Topology", config, builder.createTopology());
+
+//    	LocalCluster cluster = new LocalCluster();
+//    	cluster.submitTopology("meetup-Topology", config, builder.createTopology());
+		config.setNumWorkers(6);
+		config.setNumAckers(6);
+		config.setMaxSpoutPending(1000);
+		config.setMessageTimeoutSecs(20);
+		try {
+			StormSubmitter.submitTopology("meetup-Topology", config, builder.createTopology());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 }
