dae0d0991877098e3e6684ece076e33057aa2a49
Sharan Srivatsa <shsr7814@colorado.edu>
2019-04-07 14:12:05 -0600

    spark mac setup

---
+++ spark/setup-spark.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+git clone https://github.com/jleetutorial/python-spark-tutorial
+sudo mkdir /opt/apache-spark
+
+wget http://apache.osuosl.org/spark/spark-2.4.1/spark-2.4.1-bin-hadoop2.7.tgz
+mv spark-2.4.1-bin-hadoop2.7.tgz /opt/apache-spark
+sudo tar -xvzf /opt/spark-2.4.1-bin-hadoop2.7.tgz
+
+
+echo >> ~/.bash_profile
+echo >> ~/.bash_profile
+echo "# Spark Stuff" >> ~/.bash_profile
+echo "export SPARK_HOME=/opt/apache-spark/spark-2.4.1-bin-hadoop2.7" >> ~/.bash_profile
+echo "export JAVA_HOME=/Library/Java/Home" >> ~/.bash_profile
+echo $JAVA_HOME
+echo "export PATH=$PATH:$JAVA_HOME/bin:$SPARK_HOME/bin" >> ~/.bash_profile
+source ~/.bash_profile
+
+
