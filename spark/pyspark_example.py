eb24d5360492c3aaeb2c10e19cc69c4d46a32509
Sharan Srivatsa <shsr7814@colorado.edu>
2019-04-07 18:30:04 -0600

    spark things are working

--- spark/pyspark_example.py
+++ spark/pyspark_example.py
@@ -1,8 +1,16 @@
 from pyspark.sql import SparkSession
 
+
+print('hi');
+
+# spark = SparkSession.builder.appName("someexamplename").getOrCreate()
 spark = SparkSession \
     .builder \
     .appName("myApp") \
-    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/AdoptAPetDB.Agencies")\
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.11:2.3.0") \
+    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/AdoptAPetDB.Agencies") \
     .getOrCreate()
+
 df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+df.printSchema()
+
