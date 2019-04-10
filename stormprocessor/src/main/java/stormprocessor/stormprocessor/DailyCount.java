9b3f908fbab618b03b059b15c1b072314aa45f1f
prashilbhimani <prashilbhimani24@gmail.com>
2019-04-10 16:41:43 -0600

    Added daily buckets count

--- stormprocessor/src/main/java/stormprocessor/stormprocessor/DailyCount.java
+++ stormprocessor/src/main/java/stormprocessor/stormprocessor/DailyCount.java
@@ -3,6 +3,7 @@ package stormprocessor.stormprocessor;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOptions;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -32,21 +33,31 @@ public class DailyCount extends BaseBasicBolt {
 		parser=new JSONParser();
 	}
 
+	JSONObject getBlankBuckets(){
+		JSONObject dailyBucket=new JSONObject();
+		for(int i=0;i<24;i++)
+			dailyBucket.put(String.valueOf(i), 0);
+		return  dailyBucket;
+	}
 	public void execute(Tuple input, BasicOutputCollector collector) {
 		JSONObject rsvp= (JSONObject)  input.getValueByField("json");
 		JSONObject event= (JSONObject) rsvp.get("event");
-		System.out.println(rsvp.toJSONString());
+		JSONObject group =(JSONObject) rsvp.get("group");
+		event.put("groupDetails", group);
 
-		Document result = events.find(Filters.eq("json.name",event.get("event_name"))).first();
-	    if(result==null){
-	    	//Insert new Json in Events DB
-			Document d= new Document("event",event);
-			events.insertOne(d);
 
+		Document result = events.find(Filters.eq("event.event_name",event.get("event_name"))).first();
+	    if(result==null) {
+			//Insert new Json in Events DB
+			JSONObject dailyBuckets = getBlankBuckets();
+			event.put("dailyCounts", dailyBuckets);
+			result = new Document("event", event);
+			events.insertOne(result);
 		}else{
-	    	//Read the JSON and update it
+			events.replaceOne(Filters.eq("event.event_name",event.get("event_name") ), result, new UpdateOptions().upsert(true));
 		}
-        //System.out.println("Did this work? "+result.get("json"));
+			//Read the JSON and update it
+		//System.out.println("Did this work? "+result.get("json"));
         //Document o = (Document) result.get("json");
         //System.out.println(o.get("name"));
 	}
