87d74d9fcb63dc7e931ec9502c7b101a302bd180
Sharan Srivatsa <shsr7814@colorado.edu>
2019-04-19 16:06:14 -0600

    got simple card to work

--- reactdashboard/dashboard/src/components/MeetupAPI/EventsPage/EventsPage.js
+++ reactdashboard/dashboard/src/components/MeetupAPI/EventsPage/EventsPage.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import UserTickerCard from "./UserTickerCard/UserTickerCard"
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
+import SimpleCard from "./SimpleCard/SimpleCard";
 export default class EventsPage extends Component {
   render() {        
-    return (      
-          <Card>
-            <Typography gutterBottom variant="h5" component="h2"> Live RSVP counter </Typography>
-            <Typography gutterBottom variant="subheading" component="h2"> Some dummy value </Typography>
-          </Card>      
+    return (       
+          <SimpleCard/> 
     )
   }
 }
