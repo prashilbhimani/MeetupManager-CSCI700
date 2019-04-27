01917d71ccd949868ff12534c5e51a34461596b5
Mahesh Parab <maheshp2812@gmail.com>
2019-04-26 19:17:56 -0600

    More error handling stuff

--- reactdashboard/NodeJSServer/server.js
+++ reactdashboard/NodeJSServer/server.js
@@ -1,32 +1,32 @@
-const express = require("express")
-const cors = require("cors")
-const jsonParser = require("body-parser").json()
+const express = require("express");
+const cors = require("cors");
+const jsonParser = require("body-parser").json();
 
-const mongo = require("mongodb").MongoClient
-const url = "mongodb://35.225.229.89:27017"
+const mongo = require("mongodb").MongoClient;
+const url = "mongodb://35.225.229.89:27017";
 
 const port = 9001;
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 
 mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
   if (err) throw err;
 
   const db = client.db("newdb");
-  const events = db.collection("events")
-  const rsvps = db.collection("rsvps")
+  const events = db.collection("events");
+  const rsvps = db.collection("rsvps");
   
 
-  app.get('/:groupId/events', (req, res, next) => {
+  app.get("/:groupId/events", (req, res, next) => {
     const groupId = Number(req.params.groupId);
     events.find({"event.groupDetails.group_id" : groupId}).toArray((err, results) => {
       for(var i = 0; i < results.length; ++i) {
-        var dailyCounts = {}
+        var dailyCounts = {};
         for(var j = 0; j < 24; ++j)
           dailyCounts[j.toString()] = results[i].event.dailyCounts[(2*j).toString()] +
-            results[i].event.dailyCounts[(2*j+1).toString()]
-        results[i].event.dailyCounts = dailyCounts
+            results[i].event.dailyCounts[(2*j+1).toString()];
+        results[i].event.dailyCounts = dailyCounts;
       }
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -34,9 +34,11 @@ mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
     });
   });
 
-  app.get('/:eventId/rsvps', (req, res, next) => {
+  app.get("/:eventId/rsvps", (req, res, next) => {
     const eventId = req.params.eventId;
     rsvps.find({"json.event.event_id" : eventId}).toArray((err, results) => {
+      if(results.length == 0)
+        return res.status(404).send("No RSVPs for eventId " + eventId);
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.status(200).send(results);
@@ -46,34 +48,38 @@ mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
   app.get("/:eventId/hourbuckets", (req, res, next) => {
     const eventId = req.params.eventId;
     events.findOne({"event.event_id" : eventId}, (err, result) => { 
-      var dailyCounts = {}
+      var dailyCounts = {};
       for(var i = 0; i < 24; ++i)
         dailyCounts[i.toString()] = result.event.dailyCounts[(2*i).toString()] +
-          result.event.dailyCounts[(2*i+1).toString()]
+          result.event.dailyCounts[(2*i+1).toString()];
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.status(200).send(dailyCounts)
+      res.status(200).send(dailyCounts);
     });
   });
 
   app.get("/:eventId/rsvpcount", (req, res, next) => {
     const eventId = req.params.eventId;
     rsvps.find({"json.event.event_id" : eventId}).toArray((err, results) => {
+      if(results.length == 0) {
+        return events.findOne({"event.event_id" : eventId}, (err, result) => {
+          if(!result) return res.status(404).send("No event found with eventId " + eventId);
+          else return res.status(404).send("No RSVPs for eventId " + eventId);
+        });
+      }
       results.sort((x, y) => { return x.json.mtime - y.json.mtime; });
-      if(results.length == 0)
-        return res.status(404).send('Resource not found');
-      const ONE_DAY = 24 * 60 * 60 * 1000 // ms
-      const count = results.length
-      const min_mtime = results[0].json.mtime
-      const max_mtime = results[count - 1].json.mtime
-      var rsvps = {"total_count" : count}
+      const ONE_DAY = 24 * 60 * 60 * 1000; // ms
+      const count = results.length;
+      const min_mtime = results[0].json.mtime;
+      const max_mtime = results[count - 1].json.mtime;
+      var rsvps = {"total_count" : count};
 
       for(var i = min_mtime; i <= max_mtime; i += ONE_DAY)
         rsvps[Math.floor(i / ONE_DAY)] = {
           "start_time" : i,
           "end_time": i + ONE_DAY,
           "count" : 0
-        }
+        };
       
       for(var i = 0; i < count; ++i) {
         const mtime = results[i].json.mtime;
@@ -81,11 +87,11 @@ mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
       }
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.status(200).send(rsvps)
+      res.status(200).send(rsvps);
     });
   });
 });
 
 app.listen(port, () => {
-  console.log("CORS-enabled web server listening on port " + port)
+  console.log("CORS-enabled web server listening on port " + port);
 });
